e0e5662363455971a5ee67308844f7c97ce944b5
Bal Krishna Poudel <baalkissna2012@gmail.com>
2020-07-26 22:36:34 +0200

    Minor modifications

--- tests/check_timer.c
+++ tests/check_timer.c
@@ -68,6 +68,6 @@ int main(void) {
     srunner_run_all (sr, CK_NORMAL);
     int number_failed = srunner_ntests_failed(sr);
     srunner_free(sr);
-
+    // Some modification!
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
